792389fa7496262f382994084e94900a1c0f63cc
Sarkar, Sankha Subhra <sankha.subhra.sarkar@intel.com>
2020-08-03 16:53:18 -0700

    adding study planner and example code

---
+++ read/example.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/valid-palindrome/
+// "A man, a plan, a canal: Panama"
+// true
+class Solution {
+    string prune(const string& s){
+        string ss;
+        for(auto c : s){
+            if(c >= 'a' && c <= 'z')
+                ss += c;
+            if(c >= 'A' && c <= 'Z')
+                ss += c-'A'+'a';
+            if(c >= '0' && c <= '9')
+                ss += c;
+        }
+        return ss;
+    }
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        if(s.size()==1) return true;
+        
+        string ss = prune(s);
+        
+        int left(0);
+        int right(ss.length()-1);
+        cout << "pruned: "<< ss << endl;
+        while(left < right){
+            if(ss[left] != ss[right]){
+                cout << "left, right "<< left<<", "<<right<<", "<<ss[left]<<", "<<ss[right]<<endl;
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
+
+
+// remove duplicates
+
+class RemoveDuplicates {
+ public:
+static int remove(vector<int>& arr) {
+    int nextNonDuplicate = 1;  // index of the next non-duplicate element
+    for (int i = 1; i < arr.size(); i++) {
+      if (arr[nextNonDuplicate - 1] != arr[i]) {
+        arr[nextNonDuplicate] = arr[i];
+        nextNonDuplicate++;
+      }
+    }
+
+    return nextNonDuplicate;
+  }
+};
---
+++ read/study_planner.md
@@ -0,0 +1,15 @@
+# study planner
+
+- sliding window
+- two pointers
+- bubble sort
+- selection sort 
+- quick sort
+- linked list, slow and fast pointer
+- stack, queue
+- graph BFS
+- graph DFS
+- binary tree
+- binary search tree
+- binary search, two pointers
+- array
