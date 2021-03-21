3d3f4f5eb6ab7cd8f2c4d2366f5523eee6c59b07
Sarkar, Sankha Subhra <sankha.subhra.sarkar@intel.com>
2021-03-20 22:31:50 -0700

    another quick sort algo

--- src/Sort.cpp
+++ src/Sort.cpp
@@ -98,6 +98,50 @@ void QuickSort(int arr[], int low, int high){
         QuickSort(arr, pi+1, high);
     }
 }   
+
+i
+// another implementation of quick sort
+int partition(int *a,int start,int end)
+{
+    int pivot=a[end];
+    //P-index indicates the pivot value index
+
+    int P_index=start;
+    int i,t; //t is temporary variable
+
+    //Here we will check if array value is
+    //less than pivot
+    //then we will place it at left side
+    //by swapping
+
+    for(i=start;i<end;i++)
+    {
+    	if(a[i]<=pivot)
+        {
+            t=a[i];
+            a[i]=a[P_index];
+            a[P_index]=t;
+            P_index++;
+        }
+     }
+     //Now exchanging value of
+     //pivot and P-index
+      t=a[end];
+      a[end]=a[P_index];
+      a[P_index]=t;
+
+     //at last returning the pivot value index
+     return P_index;
+ }
+void Quicksort(int *a,int start,int end)
+{
+    if(start<end)
+    {
+        int P_index=partition(a,start,end);
+        Quicksort(a,start,P_index-1);
+        Quicksort(a,P_index+1,end);
+    }
+}
 //////////////////////////////////////////
 //////////// Merge Sort //////////////////
 //////////////////////////////////////////
